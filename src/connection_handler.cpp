6c1b1b729f4e1ae1cf86dffbcdc11031fb8f9ba1
xxlukasz11 <xxlukasz11@gmail.com>
2019-06-27 22:40:26 +0200

    change order of functions in connection handler class

--- src/connection_handler.cpp
+++ src/connection_handler.cpp
@@ -15,21 +15,6 @@ std::mutex Connection_handler::mtx_;
 
 Connection_handler::Connection_handler(Msg_parser& parser, Socket socket) : parser_(parser), socket_(socket) {}
 
-void Connection_handler::handle() {
-	using M = Msg_parser::mode;
-	auto mode = parser_.get_mode();
-
-	switch (mode) {
-		case M::insert:				handle<M::insert>();			break;
-		case M::select:				handle<M::select>();			break;
-		case M::return_tags_tree:	handle<M::return_tags_tree>();	break;
-		case M::add_tag:			handle<M::add_tag>();			break;
-		case M::delete_tag:			handle<M::delete_tag>();		break;
-
-		default: throw Unknown_message_format();
-	}
-}
-
 template<>
 void Connection_handler::handle<Msg_parser::mode::select>() {
 	auto min_date_str = parser_.next();
@@ -151,5 +136,20 @@ void Connection_handler::handle<Msg_parser::mode::delete_tag>() {
 	}
 	else {
 		socket_.send_string("Tag of id: " + query.get_tag_id() + " has been already deleted");
+	}
+}
+
+void Connection_handler::handle() {
+	using M = Msg_parser::mode;
+	auto mode = parser_.get_mode();
+
+	switch (mode) {
+	case M::insert:				handle<M::insert>();			break;
+	case M::select:				handle<M::select>();			break;
+	case M::return_tags_tree:	handle<M::return_tags_tree>();	break;
+	case M::add_tag:			handle<M::add_tag>();			break;
+	case M::delete_tag:			handle<M::delete_tag>();		break;
+
+	default: throw Unknown_message_format();
 	}
 }
