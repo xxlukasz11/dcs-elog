c882548b488779dd0807567788d0543dbf1c2cbc
xxlukasz11 <xxlukasz11@gmail.com>
2019-07-07 17:14:50 +0200

    tag update form and JS handler

--- webApp/tags_controller.js
+++ webApp/tags_controller.js
@@ -65,6 +65,27 @@ app.controller('edit_tags', function ($scope, $http, $rootScope, host) {
 		});
 	}
 
+	$scope.send_update_request = function (tag_id_, tag_name_) {
+		const params_object = {
+			tag_id: tag_id_,
+			tag_name: tag_name_
+		};
+
+		$http({
+			url: host + "update_tag.php",
+			method: "GET",
+			params: {
+				date: new Date().getTime(),
+				content: JSON.stringify(params_object)
+			}
+		}).then(function (response) {
+			$rootScope.event_log.success(response.data.message);
+			$scope.load_tags();
+		}, function (response) {
+			$rootScope.event_log.error("Cannot update '" + tag_name_ + "' tag");
+		});
+	}
+
 	$scope.add_tag = function () {
 		if ($scope.tree == null) {
 			$rootScope.event_log.error("Tree is not loaded");
@@ -114,6 +135,29 @@ app.controller('edit_tags', function ($scope, $http, $rootScope, host) {
 
 		let tag_id = $scope.tree.get_tag_id(tag_name);
 		$scope.send_delete_request(tag_id, tag_name);
+	}
+
+	$scope.update_tag = function () {
+		if ($scope.tree == null) {
+			$rootScope.event_log.error("Tree is not loaded");
+			return;
+		}
+
+		const old_tag_name = read_from_input("update_tag_input_old");
+		const new_tag_name = read_from_input("update_tag_input_new");
+
+		if (old_tag_name == "" || new_tag_name == "") {
+			$rootScope.event_log.error("Empty tag field");
+			return;
+		}
+
+		if (!$scope.tree.has_tag(old_tag_name)) {
+			$rootScope.event_log.error("Tag '" + old_tag_name + "' does not exist");
+			return;
+		}
+
+		let old_tag_id = $scope.tree.get_tag_id(old_tag_name);
+		$scope.send_update_request(old_tag_id, new_tag_name);
 	}
 	
 });
