a86f8f11ab663fc160b2780898e589b5bb8c8510
xxlukasz11 <xxlukasz11@gmail.com>
2019-08-05 23:19:42 +0200

    complete msg_parser test cases

--- tests/src/msg_parser_test.cpp
+++ tests/src/msg_parser_test.cpp
@@ -5,22 +5,37 @@ TEST(msg_parser, select_mode)
 {
 	Msg_parser parser("[1]");
 	Msg_parser::mode mode = parser.get_mode();
-	
 	EXPECT_EQ(mode, Msg_parser::mode::select);	
 }
 
 TEST(msg_parser, insert_mode) {
 	Msg_parser parser("[0]");
 	Msg_parser::mode mode = parser.get_mode();
-
 	EXPECT_EQ(mode, Msg_parser::mode::insert);
 }
 
 TEST(msg_parser, return_tags_tree_mode) {
 	Msg_parser parser("[2]");
 	Msg_parser::mode mode = parser.get_mode();
-
 	EXPECT_EQ(mode, Msg_parser::mode::return_tags_tree);
+}
+
+TEST(msg_parser, add_tag_mode) {
+	Msg_parser parser("[3]");
+	Msg_parser::mode mode = parser.get_mode();
+	EXPECT_EQ(mode, Msg_parser::mode::add_tag);
+}
+
+TEST(msg_parser, delete_tag_mode) {
+	Msg_parser parser("[4]");
+	Msg_parser::mode mode = parser.get_mode();
+	EXPECT_EQ(mode, Msg_parser::mode::delete_tag);
+}
+
+TEST(msg_parser, update_tag_mode) {
+	Msg_parser parser("[5]");
+	Msg_parser::mode mode = parser.get_mode();
+	EXPECT_EQ(mode, Msg_parser::mode::update_tag);
 }
 
 int main(int argc, char** argv){
--- tests/src/select_query_test.cpp
+++ tests/src/select_query_test.cpp
@@ -11,7 +11,9 @@ TEST(select_query, print_sql_string) {
 	query.set_max_date("2019-06-16");
 	
 	Prepared_statement p = query.create_statement();
-	std::cout << p.get_sql() << std::endl;
+	std::string s = p.get_sql();
+	EXPECT_TRUE(s.find("OR") != std::string::npos);
+	EXPECT_TRUE(s.find("AND") != std::string::npos);
 }
 
 int main(int argc, char** argv) {
