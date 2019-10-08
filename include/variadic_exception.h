d7bde9a2e3a8abcb7d3b53ed5db672a4660b2b94
xxlukasz11 <xxlukasz11@gmail.com>
2019-10-08 23:00:34 +0200

    fix expanding parameter pack in variadic exception class

--- include/variadic_exception.h
+++ include/variadic_exception.h
@@ -6,21 +6,38 @@
 #include <sstream>
 #include <utility>
 
+template<typename Base, typename Derived>
+using is_not_copy_constructor = std::enable_if_t<
+	!std::is_base_of<Base, typename std::remove_reference_t<Derived>>::value>;
+
 class Variadic_exception : public Base_exception {
 public:
-	template<typename... T>
-	Variadic_exception(T&&... args);
+	Variadic_exception() = default;
+	template<typename U, typename... T, typename = is_not_copy_constructor<Variadic_exception, U>>
+	Variadic_exception(U&& arg, T&&... args);
 	virtual const std::string& what() const noexcept override;
 
 private:
+	template<typename U, typename... T>
+	void expand_parameters(std::ostringstream& stream, U&& arg, T&&... args);
+	void expand_parameters(std::ostringstream& stream);
+
 	std::string message_;
 };
 
-template<typename... T>
-Variadic_exception::Variadic_exception(T&&... args){
+template<typename U, typename... T, typename>
+Variadic_exception::Variadic_exception(U&& arg, T&&... args){
 	std::ostringstream stream;
-	(int[]){0, ( (stream << args), 0 ) ... };
-	message_ = stream.str();
+	stream << arg;
+	expand_parameters(stream, args...);
 }
+
+template<typename U, typename ...T>
+inline void Variadic_exception::expand_parameters(std::ostringstream& stream, U&& arg, T && ...args) {
+	stream << arg;
+	expand_parameters(stream, args...);
+}
+
+
 
 #endif
--- src/variadic_exception.cpp
+++ src/variadic_exception.cpp
@@ -3,3 +3,7 @@
 const std::string& Variadic_exception::what() const noexcept {
 	return message_;
 }
+
+void Variadic_exception::expand_parameters(std::ostringstream& stream) {
+	message_ = stream.str();
+}
