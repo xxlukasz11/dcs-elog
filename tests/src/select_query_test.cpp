ccf9a324d52e3c058e834a5c8bdb2358e41b22f3
xxlukasz11 <xxlukasz11@gmail.com>
2019-11-30 14:27:36 +0100

    change variable names in select query test

--- tests/src/select_query_test.cpp
+++ tests/src/select_query_test.cpp
@@ -19,20 +19,19 @@ Select_query create_query() {
 	return query;
 }
 
-TEST(select_query, print_sql_string) {
+TEST(select_query, sql_contains_logic_operators) {
 	Select_query query = create_query();
-	Prepared_statement p = query.create_statement();
-	std::string stmt = p.get_sql();
+	Prepared_statement stmt = query.create_statement();
+	std::string sql = stmt.get_sql();
 
-	EXPECT_TRUE(stmt.find("OR") != std::string::npos);
-	EXPECT_TRUE(stmt.find("AND") != std::string::npos);
+	EXPECT_TRUE(sql.find("OR") != std::string::npos);
+	EXPECT_TRUE(sql.find("AND") != std::string::npos);
 }
 
 TEST(select_attachments_query, valid_event_id_section) {
 	Select_query query = create_query();
 	std::vector<std::string> event_id_list = { "1", "2", "11" };
 	Prepared_statement stmt = query.create_attachments_statement(event_id_list);
-	std::string sql = stmt.get_sql();
 
 	const std::string expected = "(" +
 		Prepared_statement::PARAM_PLACEHOLDER + COMMA_SPACE +
